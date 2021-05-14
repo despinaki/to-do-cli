b1dbbe34c285677244221ee7e94b0dfdee0a3175
despinaki <despoina.sifouna@kaluza.com>
2021-05-14 14:11:38 +0100

    describe database schema with Table, and populate database

--- build.sbt
+++ build.sbt
@@ -3,3 +3,10 @@ name := "to-do-cli"
 version := "0.1"
 
 scalaVersion := "2.13.5"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.slick"  %% "slick"                % "3.3.3",
+  "com.typesafe.slick"  %% "slick-hikaricp"       % "3.3.3",
+  "org.slf4j"           %  "slf4j-nop"            % "1.6.4",
+  "org.xerial"          %  "sqlite-jdbc"          % "3.34.0"
+)
--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,3 +1,35 @@
-object Hello extends App {
-  println("Hello, World!")
+import slick.jdbc.JdbcBackend.Database
+import slick.lifted.{TableQuery, Tag}
+import slick.jdbc.SQLiteProfile.api._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class ToDos (tag: Tag) extends Table[(Int, String, String)](tag, "TODOS") { //todo: replace int with the appropriate datetime type
+  def date = column[Int]("DATE", O.PrimaryKey) // This is the primary key column
+  def project = column[String]("PROJECT")
+  def item = column[String]("ITEM")
+  // Every table needs a * projection with the same type as the table's type parameter
+  def * = (date, project, item)
 }
+
+object MyDB extends App {
+  val db = Database.forConfig("todos")
+  val todosTable = TableQuery[ToDos]
+  val setup = DBIO.seq(
+    // Create the table, including primary and foreign keys
+    todosTable.schema.create,
+
+    // Insert some vlues into the table (using JDBC's batch insert feature, if supported by the DB)
+    todosTable ++= Seq(
+      (1, "homework", "study for math test"),
+      (2, "chores"  , "do laundry"),
+      (3, "homework", "finish essay")
+    )
+  )
+
+  val setupFuture = db.run(setup) //returns a promise
+  setupFuture.value
+  db.close() //close connection to server otherwise it keeps waiting forever
+}
+
+
